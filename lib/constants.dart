6c505c37d2c1feb597fc48188edbc2333f801694
Roshnai1014 <roshnaip4@gmail.com>
2025-03-27 20:02:49 +0530

    initial comment

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String baseUrl = "https://parseapi.back4app.com";
+const String appId = "FbFJiFtSZFkxLkNcmXNxAaCHwmdWmW6cwNgdWfHC";
+const String restAPIkey = "hlzsRVM3pJzfooIAN1WlFBa4v8xfVy9wGOWpXdBU";
